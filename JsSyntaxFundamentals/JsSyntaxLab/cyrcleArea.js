1aa65b5ee62e9f4b87ae7198aeebb818099c878e
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-02-25 21:47:50 +0200

    commit new task

--- JsSyntaxFundamentals/JsSyntaxLab/cyrcleArea.js
+++ JsSyntaxFundamentals/JsSyntaxLab/cyrcleArea.js
@@ -1,10 +1,10 @@
 function solve(num) {
     if (typeof num === "number") {
-        let result = Math.PI * num ** 2
-        console.log(result.toFixed(2))
+        let result = Math.PI * num ** 2;
+        console.log(result.toFixed(2));
     }
     else {
-        console.log(`We can not calculate the circle area, because we receive a ${typeof num}.`)
+        console.log(`We can not calculate the circle area, because we receive a ${typeof num}.`);
     }
 }
 
---
+++ JsSyntaxFundamentals/JsSyntaxLab/numbersFromOneToFive.js
@@ -0,0 +1,8 @@
+function solve() {
+    for (let index = 1; index < 6; index++) {
+        console.log(index)
+        
+    }
+}
+
+solve()
