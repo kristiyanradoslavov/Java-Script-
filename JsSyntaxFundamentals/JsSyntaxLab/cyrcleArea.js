59c0fbe15cad4e020ac9608c583e4d0cb4085e99
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-02-25 21:40:15 +0200

    commit new task

---
+++ JsSyntaxFundamentals/JsSyntaxLab/cyrcleArea.js
@@ -0,0 +1,12 @@
+function solve(num) {
+    if (typeof num === "number") {
+        let result = Math.PI * num ** 2
+        console.log(result.toFixed(2))
+    }
+    else {
+        console.log(`We can not calculate the circle area, because we receive a ${typeof num}.`)
+    }
+}
+
+
+solve(5)
