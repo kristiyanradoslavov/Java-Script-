7935cfa2f0e32070753474b348a91bb6a6399223
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-02-27 22:05:02 +0200

    commit new changes

---
+++ JsSyntaxFundamentals/JsSyntaxExercise/arrayRotation.js
@@ -0,0 +1,17 @@
+function rotation(array, numberRotation) {
+
+    for (let i = 0; i < numberRotation; i++){
+        let number = array.shift()
+        array.push(number)
+        
+    }
+
+    console.log(array.join(" "))
+}
+
+
+rotation ([51, 47, 32, 61, 21], 2)
+
+rotation([32, 21, 61, 1], 4)
+
+rotation([2, 4, 15, 31], 5)
