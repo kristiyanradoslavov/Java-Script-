fbb3ac9da966025c15a689b98f37d04876e6e18c
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-03-02 23:41:16 +0200

    commit new task

--- functionsAndStatements/functionsAndStatementsExercise/palindromIntegers.js
+++ functionsAndStatements/functionsAndStatementsExercise/palindromIntegers.js
@@ -1,1 +1,17 @@
-console.log("Hello")
+function palindromes(numbers) {
+    let finalResult = []
+
+    for (const num of numbers) {
+        let newNumber = num.toString()
+                            .split("")
+                            .reverse()
+                            .join("")
+
+        finalResult.push(Number(newNumber) === num)
+    }
+
+    return finalResult.join("\n")
+}
+
+
+console.log(palindromes([32,2,232,1010]))
