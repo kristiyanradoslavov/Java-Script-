6803c477d9ad90eeaaa420fd17fea87c5d16ba88
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-03-08 19:52:19 +0200

    commit new task

---
+++ ObjectsAndClasses/ObjectsAndClasses - Lab/01. Person-info.js
@@ -0,0 +1,13 @@
+function person(firstName, lastName, age) {
+    let result = { firstName, lastName, age };
+
+    return(result)
+}
+
+
+console.log(person(
+    "Peter",
+    "Pan",
+    "20"
+
+))
