7832ee5245a10f50e85c3b5006b85802c609f9f6
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-03-09 22:17:33 +0200

    commit new task

--- ObjectsAndClasses/ObjectsAndClasses - Lab/05 convert-to-Json.js
+++ ObjectsAndClasses/ObjectsAndClasses - Lab/05 convert-to-Json.js
@@ -1,1 +1,11 @@
-function convert(firstName, lastName, )
+function convert(name, lastName, hairColor){
+    let resultObj = {
+        name,
+        lastName,
+        hairColor
+    }
+
+    console.log(JSON.stringify(resultObj))
+}
+
+convert('George', 'Jones', 'Brown')
