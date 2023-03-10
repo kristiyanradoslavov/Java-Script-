c7a8a6b28ab405780d322ffec6bdaf4af7984d01
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-03-11 00:00:02 +0200

    commit new task

---
+++ ObjectsAndClasses/ObjectsAndClasses - Exercise/02. Towns.js
@@ -0,0 +1,24 @@
+function towns(input) {
+
+    for (const i of input) {
+        let [town, latitude, longitude] = i.split(" | ");
+        latitude = Number(latitude).toFixed(2);
+        longitude = Number(longitude).toFixed(2);
+
+        let result = {
+            town, 
+            latitude,
+            longitude
+        };
+
+        console.log(result);
+    }
+
+}
+
+
+towns(
+    ['Sofia | 42.696552 | 23.32601',
+        'Beijing | 39.913818 | 116.363625']
+
+)
