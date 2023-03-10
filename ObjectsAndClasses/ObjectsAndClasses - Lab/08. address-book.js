3f692e6df7665570fbd5c56b85586db01ceb0016
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-03-10 20:19:52 +0200

    commit new task

---
+++ ObjectsAndClasses/ObjectsAndClasses - Lab/08. address-book.js
@@ -0,0 +1,30 @@
+function addresses(input) {
+
+    let addressObject = {};
+
+    for (const info of input) {
+        let [firstName, address] = info.split(":");
+        addressObject[firstName] = address;
+    }
+
+    let result = []
+
+    for (const [key, value] of Object.entries(addressObject)) {
+        result.push(`${key} -> ${value}`)
+    }
+
+    let sortedInput = [...result].sort((a, b) => a.localeCompare(b));
+
+
+    for (let i of sortedInput) {
+        console.log(i)
+    }
+
+}
+
+addresses(
+    ['Tim:Doe Crossing',
+    'Bill:Nelson Place',
+    'Peter:Carlyle Ave',
+    'Bill:Ornery Rd']
+)
