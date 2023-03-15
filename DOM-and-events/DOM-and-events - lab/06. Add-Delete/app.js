3b10dcc0609ecc5cdcf1e54dc3eb92151c0f3483
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-03-15 23:13:00 +0200

    commit new task

--- DOM-and-events/DOM-and-events - lab/06. Add-Delete/app.js
+++ DOM-and-events/DOM-and-events - lab/06. Add-Delete/app.js
@@ -1,3 +1,19 @@
 function addItem() {
-    //TODO...
+    const newtText = document.getElementById("newItemText").value
+
+    const unorderedList = document.getElementById("items");
+    let newItem = document.createElement("li");
+    newItem.textContent = newtText;
+    unorderedList.appendChild(newItem);
+
+    let newAnchor = document.createElement("a");
+    newAnchor.textContent = "[Delete]";
+    newAnchor.href = "#"
+    newItem.appendChild(newAnchor)
+
+    newItem.addEventListener("click", eventHandler);
+
+    function eventHandler(){
+        newItem.remove();
+    }
 }
