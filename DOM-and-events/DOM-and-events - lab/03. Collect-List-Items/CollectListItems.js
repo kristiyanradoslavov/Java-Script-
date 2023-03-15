4788e26e04129ec92367e1d8adc2cd08e2f947db
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-03-15 22:01:25 +0200

    commit new task

--- DOM-and-events/DOM-and-events - lab/03. Collect-List-Items/CollectListItems.js
+++ DOM-and-events/DOM-and-events - lab/03. Collect-List-Items/CollectListItems.js
@@ -1,3 +1,12 @@
 function extractText() {
-    // TODO
+    const content = document.getElementsByTagName("li");
+    const resultArea = document.getElementById("result")
+
+    let result = [];
+
+    for (const currentLi of content) {
+        result.push(currentLi.textContent)
+    }
+
+    resultArea.value = result.join("\n")
 }
