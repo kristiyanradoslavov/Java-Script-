d2192d5c8c3f6bed30b00257d9b855bfe0daa0cc
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-03-15 22:46:37 +0200

    commit new task

--- DOM-and-events/DOM-and-events - lab/05. Delete-From-Table/app.js
+++ DOM-and-events/DOM-and-events - lab/05. Delete-From-Table/app.js
@@ -1,3 +1,17 @@
 function deleteByEmail() {
-    console.log('TODO:...');
-}
+    const inputText = document.querySelector("label input");
+    const allEmails = document.querySelectorAll("tbody tr :nth-child(2)");
+    const resultInfo = document.getElementById("result");
+    
+    for (const currentRow of allEmails) {
+        if (currentRow.textContent === inputText.value) {
+            currentRow.parentElement.remove();
+            resultInfo.textContent = "Deleted";
+            inputText.value = ""
+            break
+        }
+
+        resultInfo.textContent = "Not found."
+    }
+
+}   
