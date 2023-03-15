3770b0d0dafb07d5d0779a26e77f9dbc254c5eec
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-03-15 20:58:08 +0200

    commit finished task

--- ObjectsAndClasses/ObjectsAndClasses - MoreExercises/01. Class-storage.js
+++ ObjectsAndClasses/ObjectsAndClasses - MoreExercises/01. Class-storage.js
@@ -11,26 +11,36 @@ class Storage {
         this.capacity -= product.quantity;
         this.totalCost += this.storage[this.storage.length - 1].price * this.storage[this.storage.length - 1].quantity
     }
+
+    getProducts() {
+        let result = [];
+
+        for (const currentObj of this.storage) {
+            result.push(JSON.stringify(currentObj))
+        }
+
+        return result.join("\n")
+    } 
 }
 
 
-let productOne = { name: 'Cucamber', price: 1.50, quantity: 15 };
-let productTwo = { name: 'Tomato', price: 0.90, quantity: 25 };
-let productThree = { name: 'Bread', price: 1.10, quantity: 8 };
-let storage = new Storage(50);
-storage.addProduct(productOne);
-storage.addProduct(productTwo);
-storage.addProduct(productThree);
-console.log(storage.getProducts());
-console.log(storage.capacity);
-console.log(storage.totalCost);
-
-
-
-// let productOne = {name: 'Tomato', price: 0.90, quantity: 19};
-// let productTwo = {name: 'Potato', price: 1.10, quantity: 10};
-// let storage = new Storage(30);
+// let productOne = { name: 'Cucamber', price: 1.50, quantity: 15 };
+// let productTwo = { name: 'Tomato', price: 0.90, quantity: 25 };
+// let productThree = { name: 'Bread', price: 1.10, quantity: 8 };
+// let storage = new Storage(50);
 // storage.addProduct(productOne);
 // storage.addProduct(productTwo);
+// storage.addProduct(productThree);
+// console.log(storage.getProducts());
+// console.log(storage.capacity);
 // console.log(storage.totalCost);
+
+
+
+let productOne = {name: 'Tomato', price: 0.90, quantity: 19};
+let productTwo = {name: 'Potato', price: 1.10, quantity: 10};
+let storage = new Storage(30);
+storage.addProduct(productOne);
+storage.addProduct(productTwo);
+console.log(storage.totalCost);
 
