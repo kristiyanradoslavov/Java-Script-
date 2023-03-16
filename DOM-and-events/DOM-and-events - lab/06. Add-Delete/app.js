43d9af0ef94784e9c19ee3cc883a6c2ff2d44842
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-03-16 19:19:14 +0200

    commit new changes

--- DOM-and-events/DOM-and-events - lab/06. Add-Delete/app.js
+++ DOM-and-events/DOM-and-events - lab/06. Add-Delete/app.js
@@ -1,17 +1,19 @@
 function addItem() {
-    const newtText = document.getElementById("newItemText").value
+    const newtText = document.getElementById("newItemText");
 
     const unorderedList = document.getElementById("items");
     let newItem = document.createElement("li");
-    newItem.textContent = newtText;
+    newItem.textContent = newtText.value;
     unorderedList.appendChild(newItem);
 
     let newAnchor = document.createElement("a");
     newAnchor.textContent = "[Delete]";
     newAnchor.href = "#"
-    newItem.appendChild(newAnchor)
+    newItem.appendChild(newAnchor);
+    
+    newtText.value = ""
 
-    newItem.addEventListener("click", eventHandler);
+    newAnchor.addEventListener("click", eventHandler);
 
     function eventHandler(){
         newItem.remove();
