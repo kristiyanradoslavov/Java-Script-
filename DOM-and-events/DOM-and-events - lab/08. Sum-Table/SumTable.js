4c8d371b68c92ff10b27b948d5cfaa295967481e
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-03-16 19:54:46 +0200

    commit new task

--- DOM-and-events/DOM-and-events - lab/08. Sum-Table/SumTable.js
+++ DOM-and-events/DOM-and-events - lab/08. Sum-Table/SumTable.js
@@ -1,3 +1,11 @@
 function sumTable() {
+    const amounts = Array.from(document.querySelectorAll("table tbody tr :nth-child(2)"));
+    amounts.shift()
+    const result = amounts.pop();
 
-}
+    let finalAmount = 0;
+
+    amounts.forEach(element => finalAmount += Number(element.textContent));
+
+    result.textContent = finalAmount;
+}   
