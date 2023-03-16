02b8230b24ee1a7ac11b876c2cb8437538b65fe8
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-03-16 20:30:02 +0200

    commit new task

--- DOM-and-events/DOM-and-events - lab/09. Highlight-Active/app.js
+++ DOM-and-events/DOM-and-events - lab/09. Highlight-Active/app.js
@@ -1,3 +1,15 @@
 function focused() {
-    console.log('TODO:...');
+    const allInputs = Array.from(document.querySelectorAll("div div input"))
+    
+    allInputs.forEach(currentInput => currentInput.addEventListener("focus", focusHandler))
+    allInputs.forEach(currentInput => currentInput.addEventListener("blur", blurHandler))
+
+    function focusHandler(event) {
+        let result = event.target;
+        result.parentElement.classList.add("focused")
+    }
+
+    function blurHandler (event) {
+        event.target.parentElement.classList.remove("focused")
+    }
 }
