0f59cedb25530b2971b2a395dba92810ab5feb52
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-03-17 22:51:16 +0200

    commit new task

--- DOM-and-events/DOM-and-events - exercise/02. Sections/script.js
+++ DOM-and-events/DOM-and-events - exercise/02. Sections/script.js
@@ -1,3 +1,19 @@
 function create(words) {
-   console.log('TODO:...');
+   const currentParent = document.getElementById("content");
+
+   for (const currentString of words) {
+      let newDiv = document.createElement("div");
+      let newParagraph = document.createElement("p");
+      newParagraph.textContent = currentString;
+      newDiv.appendChild(newParagraph);
+
+      newDiv.addEventListener("click", eventHandler);
+
+      currentParent.appendChild(newDiv)
+   }
+
+   function eventHandler(e) {
+      e.target.style.display = "none"
+   }
+
 }
