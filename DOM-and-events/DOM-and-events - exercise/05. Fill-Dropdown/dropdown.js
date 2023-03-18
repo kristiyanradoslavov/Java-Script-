fcb529732802eed29a5a0de8519e2f847fe2b7a1
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-03-18 15:12:51 +0200

    commit new task

--- DOM-and-events/DOM-and-events - exercise/05. Fill-Dropdown/dropdown.js
+++ DOM-and-events/DOM-and-events - exercise/05. Fill-Dropdown/dropdown.js
@@ -1,3 +1,13 @@
 function addItem() {
-    console.log('TODO:...');
+    const dropDownMenu = document.getElementById("menu");
+    const [text, value, btn] = document.getElementsByTagName("input");
+
+    let newOption = document.createElement("option");
+    newOption.textContent = text.value;
+    newOption.value = value.value;
+
+    dropDownMenu.appendChild(newOption)
+
+    text.value = "";
+    value.value = "";
 }
