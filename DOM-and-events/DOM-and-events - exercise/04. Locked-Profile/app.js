922ce362a9ce4c697a312c84210b893a55871f25
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-03-18 14:30:01 +0200

    commit new tasks

--- DOM-and-events/DOM-and-events - exercise/04. Locked-Profile/app.js
+++ DOM-and-events/DOM-and-events - exercise/04. Locked-Profile/app.js
@@ -1,3 +1,24 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const allProfiles = Array.from(document.querySelectorAll("#main > div"))
+    const buttons = Array.from(document.querySelectorAll(".profile button"));
+
+    for (const currentButton of buttons) {
+        currentButton.addEventListener("click", eventHandler);
+    }
+
+
+    function eventHandler(e) {
+        const currentButton = e.currentTarget
+        const currentProfile = e.currentTarget.parentElement;
+        const currentDiv = currentProfile.children[9];
+        const currentInput = currentProfile.children[4]
+
+        if (currentInput.checked && currentButton.textContent === "Show more") {
+            currentDiv.style.display = "block";
+            currentButton.textContent = "Hide it";
+        } else if (currentInput.checked && currentButton.textContent === "Hide it") {
+            currentDiv.style.display = "None";
+            currentButton.textContent = "Show more"
+        }
+    }
 }
