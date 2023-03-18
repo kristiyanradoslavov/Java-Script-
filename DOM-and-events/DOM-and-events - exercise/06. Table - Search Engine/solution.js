24bcd981b51ca5fa4a994b859c651ec997bd3bb7
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-03-18 16:20:58 +0200

    commit new task

--- DOM-and-events/DOM-and-events - exercise/06. Table - Search Engine/solution.js
+++ DOM-and-events/DOM-and-events - exercise/06. Table - Search Engine/solution.js
@@ -2,7 +2,25 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
 
+      const allRows = Array.from(document.querySelectorAll("tbody tr"))
+      for (const currentRow of allRows) {
+         if (currentRow.classList == "select") {
+            currentRow.classList = ""
+         }
+      }
+
+      const allInputs = Array.from(document.querySelectorAll("tbody tr td"));
+      const currentInput = document.getElementById("searchField")
+
+      for (const i of allInputs) {
+         if (i.textContent.toLowerCase().includes(currentInput.value.toLowerCase()) && currentInput.value !== "") {
+            const parent = i.parentElement;
+            parent.classList = "select"
+            console.log()
+         }
+      }
+
+      currentInput.value = ""
    }
 }
