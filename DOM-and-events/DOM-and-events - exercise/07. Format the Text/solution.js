328c94df6cc9b79c1dfc9d724f977c9ad04de247
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-03-19 12:43:12 +0200

    commit new task

--- DOM-and-events/DOM-and-events - exercise/07. Format the Text/solution.js
+++ DOM-and-events/DOM-and-events - exercise/07. Format the Text/solution.js
@@ -1,3 +1,17 @@
 function solve() {
-  //TODO
-}
+  const currentInput = document.getElementById("input");
+  const sentences = currentInput.value.split(".");
+  const currentOutput = document.getElementById("output");
+  sentences.pop()
+
+  let sentCounter = 0;
+
+  while (sentences.length > 0) {
+    let currentParagraph = sentences.splice(0, 3)
+      .map((a) => a.trimStart())
+    
+    let newP = document.createElement("p");
+    newP.textContent = currentParagraph.join(".") + ".";
+    currentOutput.appendChild(newP)
+  }
+} 
