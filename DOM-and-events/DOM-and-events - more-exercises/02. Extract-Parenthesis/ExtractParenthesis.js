53ec590a617ac0577dfbadd2cf358fcb7542caad
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-03-20 19:48:09 +0200

    commit new task

--- DOM-and-events/DOM-and-events - more-exercises/02. Extract-Parenthesis/ExtractParenthesis.js
+++ DOM-and-events/DOM-and-events - more-exercises/02. Extract-Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,17 @@
 function extract(content) {
+    let regexPattern = /\(([\w]+ ?)*\)/g;
+    let replacePattern = new RegExp(/\(|\)/, "g");
+
+    let paragraph = document.getElementById(content)
+    let result = paragraph.textContent.match(regexPattern);
+
+    let finalResult = []
+
+    for (let i = 0; i < result.length; i++){
+        result[i] = result[i].replace(replacePattern, "")
+        finalResult.push(result[i])
+    }
+
+    return finalResult.join("; ")
 
 }
