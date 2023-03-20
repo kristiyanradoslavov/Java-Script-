63ff2cc42d5d9e8d00e6929cb35a355533987202
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-03-20 20:23:49 +0200

    commit new task

--- DOM-and-events/DOM-and-events - more-exercises/03. Mouse-Gradient/app.js
+++ DOM-and-events/DOM-and-events - more-exercises/03. Mouse-Gradient/app.js
@@ -1,3 +1,16 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const div = document.getElementById("gradient");
+    const result = document.getElementById("result");
+
+    div.addEventListener("mousemove", moveHandler);
+    div.addEventListener("mouseout", () => {result.textContent = ""});
+
+    function moveHandler(e) {
+        let event = e.offsetX;
+        if (event === 299){
+            event = 300;
+        }
+
+        result.textContent = `${Math.floor(event / 3)}%`;
+    }
 }
