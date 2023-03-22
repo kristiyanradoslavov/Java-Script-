2eff5a81878fdb713b92268a9cb3e53049058f44
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-03-22 23:32:26 +0200

    commit new task

--- HTTP & Rest/05. Github-Repos-by-Username/scripts/app.js
+++ HTTP & Rest/05. Github-Repos-by-Username/scripts/app.js
@@ -1,3 +1,31 @@
 function loadRepos() {
-	console.log("TODO...");
-}
+	const currentUl = document.getElementById("repos");
+	const currentUsername = document.getElementById("username").value;
+	const URI = "https://api.github.com/users"
+
+	fetch(`${URI}/${currentUsername}/repos`)
+		.then((result) => result.json())
+		.then((res) => {
+			let allChildren = Array.from(currentUl.children)
+				.map((current) => current.remove())
+
+			for (const currentRepo of res) {
+				let newList = document.createElement("li");
+				let newATag = document.createElement("a");
+				let currentUrl = currentRepo.html_url;
+				let currentName = currentRepo.full_name;
+
+				newATag.href = currentUrl;
+				newATag.textContent = currentName;
+				newList.appendChild(newATag);
+				currentUl.appendChild(newList);
+			}
+		})
+		.catch((error) => {
+			let newList = document.createElement("li");
+			newList.textContent = error;
+			currentUl.appendChild(newList)
+
+		})
+
+}	
