02396b53ad838a94e7c6e1eecd958327ff7f9cfe
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-03-22 21:00:11 +0200

    commit new task

--- HTTP & Rest/04. GitHub-Repos/scripts/app.js
+++ HTTP & Rest/04. GitHub-Repos/scripts/app.js
@@ -1,3 +1,13 @@
 function loadRepos() {
-   console.log("TODO...");
+   const resultDiv = document.getElementById('res');
+   const URI = "https://api.github.com/users/testnakov/repos12";
+
+   fetch(URI, { method: "Get" })
+    .then((result) => result.text())
+     .then((res) => {
+      resultDiv.textContent = res;
+     })
+     .catch((err) => {
+         console.error(err)
+      })
 }
