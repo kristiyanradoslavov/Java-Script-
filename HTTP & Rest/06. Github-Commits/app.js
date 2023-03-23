04d9c5b4f9f859d745b25b7de6666b081c9975d1
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-03-23 22:02:37 +0200

    commit new task

--- HTTP & Rest/06. Github-Commits/app.js
+++ HTTP & Rest/06. Github-Commits/app.js
@@ -1,4 +1,21 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    const nameInput = document.getElementById("username").value;
+    const repoName = document.getElementById("repo").value;
+    const resultUl = document.getElementById("commits");
+    const currentURI = "https://api.github.com/repos";
+
+    fetch(`${currentURI}/${nameInput}/${repoName}/commits`)
+        .then((result) => result.json())
+        .then((data) => {
+            let newList = document.createElement("li");
+            for (const currentEl of data) {
+                console.log(currentEl)
+                // let currentName = currentEl.author.name;
+                // let message = currentEl.commits.message;
+                // newList.textContent = `${currentName}:${message}`
+            };
+        })
+        .catch((error) => {
+            console.error("Ebasi")
+        })
 }
