7fc885ce807cd116195319750841d79f6c43c1ac
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-03-25 19:47:24 +0200

    commit new task

--- HTTP & Rest/HTTP & Rest - Exersice/02.Bus-Schedule/app.js
+++ HTTP & Rest/HTTP & Rest - Exersice/02.Bus-Schedule/app.js
@@ -1,10 +1,44 @@
 function solve() {
+    const departBtn = document.getElementById("depart");
+    const arriveBtn = document.getElementById("arrive");
+    const resulting = document.querySelector("#info > span");
+    const URL = "http://localhost:3030/jsonstore/bus/schedule/";
+    let busStop = "depot";
+    let previousStop = ""
+
     function depart() {
-        // TODO: 
+        fetch(`${URL}${busStop}`)
+            .then((info) => info.json())
+            .then((data) => {
+                let currentName = data.name;
+                previousStop = currentName;
+                let nextStop = data.next;
+                busStop = nextStop;
+                resulting.textContent = `Next stop ${currentName}`
+                departBtn.disabled = true;
+                arriveBtn.disabled = false;
+            })
+            .catch (() => {
+                resulting.textContent = "Error"
+                departBtn.disabled = true;
+                arriveBtn.disabled = true;
+            })
+            
     }
 
     async function arrive() {
-        // TODO:
+        try {
+            departBtn.disabled = false;
+            arriveBtn.disabled = true;
+            resulting.textContent = `Arriving at ${previousStop}`
+        }
+        catch (err) {
+            resulting.textContent = "Error";
+            departBtn.disabled = false;
+            arriveBtn.disabled = false;
+        }
+
+        
     }
 
     return {
