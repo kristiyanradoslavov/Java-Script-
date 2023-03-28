f91b944ce646839d7d8c3dcabd1bd7bf459739fe
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-03-28 21:25:15 +0300

    commit new task

--- HTTP & Rest/HTTP & Rest - Exersice/06.Phonebook/app.js
+++ HTTP & Rest/HTTP & Rest - Exersice/06.Phonebook/app.js
@@ -1,5 +1,71 @@
 function attachEvents() {
-    // TODO:
+    const BASE_URL = "http://localhost:3030/jsonstore/phonebook/";
+    const loadBtn = document.getElementById("btnLoad");
+    const createBtn = document.getElementById("btnCreate");
+    const personInput = document.getElementById("person");
+    const phoneInput = document.getElementById("phone");
+    const resultUl = document.getElementById("phonebook");
+
+    loadBtn.addEventListener("click", loadEventHandler);
+    createBtn.addEventListener("click", createEventHandler);
+
+
+    function loadEventHandler() {
+        resultUl.innerHTML = "";
+        fetch(BASE_URL)
+            .then((getResult) => getResult.json())
+            .then((getData) => {
+                for ({ person, phone, _id } of Object.values(getData)) {
+                    let newLi = document.createElement("li");
+                    let newDeleteBtn = document.createElement("button");
+                    newDeleteBtn.textContent = "Delete";
+                    newLi.innerHTML = `${person}: ${phone}`;
+                    newDeleteBtn.id = _id;
+                    newLi.appendChild(newDeleteBtn);
+                    resultUl.appendChild(newLi);
+                    newDeleteBtn.addEventListener("click", deleteEventHandler);
+                }
+            })
+            .catch(() => {
+                console.error("Error on get request")
+            })
+    }
+
+    function deleteEventHandler() {
+        const id = this.id;
+        const httpHeaders = {
+            method: "DELETE"
+        };
+        fetch(`${BASE_URL}${id}`, httpHeaders)
+            .then((result) => result.json())
+            .then((delData) => {
+                loadEventHandler();
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+    }
+
+    function createEventHandler() {
+        let postInformation = {
+            person: personInput.value,
+            phone: phoneInput.value,
+        }
+
+        fetch(BASE_URL, {
+            method: "POST",
+            body: JSON.stringify(postInformation)
+        })
+            .then(() => {
+                loadEventHandler()
+                personInput.value = "";
+                phoneInput.value = "";
+            })
+            .catch(() => {
+                console.error("error on post request")
+            })
+
+    }
 }
 
 attachEvents();
