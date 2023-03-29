438db9b509b7fc5016a7e43746acec1606388c7d
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-03-29 23:37:37 +0300

    commit new task

--- HTTP & Rest/HTTP & Rest - Exersice/08.Book-library/app.js
+++ HTTP & Rest/HTTP & Rest - Exersice/08.Book-library/app.js
@@ -1,5 +1,20 @@
 function attachEvents() {
-  // TODO:
+  const BASE_URL = "http://localhost:3030/jsonstore/collections/books/";
+  const loadBtn = document.getElementById("loadBooks");
+
+
+  loadBtn.addEventListener("click", getEventHandler);
+
+
+  function getEventHandler() {
+    fetch(BASE_URL)
+      .then((getResult) => getResult.json())
+      .then((getData) => {
+        for ([key, { author, title }] of Object.entries(getData)) {
+
+        }
+      })
+  }
 }
 
 attachEvents();
