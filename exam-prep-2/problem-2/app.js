fdeace3bc58e678e8b50350bd82af6b87e0e5580
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-04-05 23:01:07 +0300

    commit new changes

--- exam-prep-2/problem-2/app.js
+++ exam-prep-2/problem-2/app.js
@@ -1,5 +1,40 @@
 window.addEventListener('load', solve);
 
 function solve() {
-    //TODO
+    const genre = document.getElementById("genre");
+    const songName = document.getElementById("name");
+    const author = document.getElementById("author");
+    const date = document.getElementById("date");
+    const addBtn = document.getElementById("add-btn");
+    const songList = document.getElementById("all-hits-container");
+
+    addBtn.addEventListener("click", addHandler);
+
+    function addHandler(event) {
+        event.preventDefault();
+        
+        if (genre && songName && author && date) {
+
+        }
+    }
+
+    function addElement(element, parent, text, classInfo, imgSrc) {
+        let newElement = document.createElement(element);
+
+        if (text) {
+            newElement.textContent = text;
+        }
+
+        if (classInfo) {
+            newElement.className = classInfo;
+        }
+
+        if (imgSrc) {
+            newElement.src = imgSrc
+        }
+
+        parent.appendChild(newElement);
+
+        return newElement;
+    }
 }
