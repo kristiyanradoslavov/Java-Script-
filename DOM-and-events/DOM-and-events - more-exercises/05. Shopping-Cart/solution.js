e109100a6f568b455bee1dd3422d3e171f3cfc51
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-04-07 15:02:53 +0300

    commit new task

--- DOM-and-events/DOM-and-events - more-exercises/05. Shopping-Cart/solution.js
+++ DOM-and-events/DOM-and-events - more-exercises/05. Shopping-Cart/solution.js
@@ -1,3 +1,36 @@
 function solve() {
-   //TODO...
+   const addBtn = Array.from(document.getElementsByClassName("add-product"));
+   const textArea = document.getElementsByTagName("textarea")[0];
+   const checkOutBtn = document.getElementsByClassName("checkout")[0];
+
+   let products = {
+      allProducts: [],
+      totalPrice: 0,
+   };
+
+   checkOutBtn.addEventListener("click", checkOutHandler)
+
+   for (const currentBtn of addBtn) {
+      currentBtn.addEventListener("click", addHandler);
+   }
+
+   function addHandler() {
+      const mainParent = this.parentNode.parentNode;
+      const name = mainParent.querySelector(":nth-child(2)>div.product-title");
+      const price = Number(mainParent.querySelector(":nth-child(4)").textContent);
+      if (!products.allProducts.includes(name.textContent)) {
+         products.allProducts.push(name.textContent)
+      }
+      products.totalPrice += price;
+      textArea.value += `Added ${name.textContent} for ${price.toFixed(2)} to the cart.\n`
+   }
+
+   function checkOutHandler() {
+      textArea.value += `You bought ${products.allProducts.join(", ")} for ${products.totalPrice.toFixed(2)}.`
+      checkOutBtn.disabled = true;
+
+      for (const currentBtn of addBtn) {
+         currentBtn.disabled = true;
+      }
+   }
 }
