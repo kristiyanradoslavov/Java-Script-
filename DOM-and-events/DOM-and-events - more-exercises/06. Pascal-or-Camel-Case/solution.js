0668ed703fc997d8c9027b27f9cac32b53ac374e
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-04-12 20:11:51 +0300

    commit new task

--- DOM-and-events/DOM-and-events - more-exercises/06. Pascal-or-Camel-Case/solution.js
+++ DOM-and-events/DOM-and-events - more-exercises/06. Pascal-or-Camel-Case/solution.js
@@ -1,3 +1,33 @@
 function solve() {
-  //TODO...
+  const input = document.getElementById("text").value;
+  const condition = document.getElementById("naming-convention").value;
+  const spanResult = document.getElementById("result");
+
+
+  let inputWords = input.split(" ");
+  let result = [];
+  let lowerWord;
+
+  if (condition === "Camel Case") {
+    for (let index = 0; index < inputWords.length; index++) {
+      lowerWord = inputWords[index].toLowerCase();
+
+      if (index != 0) {
+        lowerWord = lowerWord.replace(lowerWord[0], lowerWord[0].toUpperCase())
+      }
+      result.push(lowerWord)
+    }
+
+  } else if (condition === "Pascal Case") {
+    for (let index = 0; index < inputWords.length; index++) {
+      lowerWord = inputWords[index].toLowerCase();
+      lowerWord = lowerWord.replace(lowerWord[0], lowerWord[0].toUpperCase())
+      result.push(lowerWord)
+    }
+    
+  } else {
+    result.push("Error!")
+  }
+
+  spanResult.textContent = result.join("")
 }
