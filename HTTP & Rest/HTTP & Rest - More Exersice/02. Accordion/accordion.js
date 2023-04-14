07a728e13c5eabf8dbb38ae4ee194a6012efc633
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-04-14 17:51:50 +0300

    commit new task

--- HTTP & Rest/HTTP & Rest - More Exersice/02. Accordion/accordion.js
+++ HTTP & Rest/HTTP & Rest - More Exersice/02. Accordion/accordion.js
@@ -1,3 +1,39 @@
 function solution() {
-    //TODO .....
+    let BASE_URL = "http://localhost:3030/jsonstore/advanced/articles/list/";
+    let mainElement = document.getElementById("main");
+
+    window.addEventListener("load", loadHandler);
+
+    function loadHandler() {
+        fetch(BASE_URL)
+            .then((getResult) => getResult.json())
+            .then((getData) => {
+                for (const currentObj of Object.values(getData)) {
+
+                }
+            })
+    }
+
+
+    function addElement(element, parent, text, classInfo, idInfo) {
+        let newElement = document.createElement(element);
+
+        if (text) {
+            newElement.textContent = text;
+        }
+
+        if (classInfo) {
+            newElement.classList.add(...classInfo);
+        }
+
+        if (idInfo) {
+            newElement.id = idInfo;
+        }
+
+        parent.appendChild(newElement);
+        return newElement;
+    }
+
 }
+
+solution()
