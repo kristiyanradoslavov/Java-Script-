70a800f31596ce7500f75da8e981d2b7834473d9
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-04-14 18:23:52 +0300

    commit new task

--- HTTP & Rest/HTTP & Rest - More Exersice/02. Accordion/accordion.js
+++ HTTP & Rest/HTTP & Rest - More Exersice/02. Accordion/accordion.js
@@ -1,5 +1,6 @@
 function solution() {
     let BASE_URL = "http://localhost:3030/jsonstore/advanced/articles/list/";
+    let DETAILS_URL = "http://localhost:3030/jsonstore/advanced/articles/details/"
     let mainElement = document.getElementById("main");
 
     window.addEventListener("load", loadHandler);
@@ -9,9 +10,36 @@ function solution() {
             .then((getResult) => getResult.json())
             .then((getData) => {
                 for (const currentObj of Object.values(getData)) {
+                    let wrapDiv = addElement("div", mainElement, null, ["accordion"]);
+                    let headDiv = addElement("div", wrapDiv, null, ["head"]);
+                    addElement("span", headDiv, currentObj.title);
+                    let newBtn = addElement("button", headDiv, "More", ["button"], currentObj._id);
+                    let extraDiv = addElement("div", wrapDiv, null, ["extra"]);
+                    fetch(`${DETAILS_URL}${currentObj._id}`)
+                        .then((detailsResult) => detailsResult.json())
+                        .then((detailsData) => {
+                            addElement("p", extraDiv, `${detailsData.content}`);
 
+                        })
+                        .catch((error) => console.error(error))
+
+                    newBtn.addEventListener("click", btnHandler);
                 }
             })
+            .catch((error) => console.error(error))
+    }
+
+    function btnHandler() {
+        let parent = this.parentNode.parentNode;
+        let extra = parent.querySelector(".extra");
+
+        if (this.textContent === "More") {
+            extra.style.display = "block";
+            this.textContent = "Less";
+        } else {
+            extra.style.display = "none";
+            this.textContent = "More";
+        }
     }
 
 
