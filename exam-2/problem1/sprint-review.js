de1226564ec94377384e460327aa56ca8efe82b0
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-04-19 13:03:16 +0300

    commit new task

--- exam-2/problem1/sprint-review.js
+++ exam-2/problem1/sprint-review.js
@@ -68,20 +68,49 @@ function solve(input) {
 
         allTasks[assignee].splice(index, 1);
     }
+
+    let totalPoints = {
+        ToDo: 0,
+        InProgress: 0,
+        CodeReview: 0,
+        Done: 0,
+    }
+
+    for (const currentAssignee of Object.values(allTasks)) {
+        for (const currentSprint of currentAssignee) {
+            let currentStatus = currentSprint.status.split(" ").join("");
+            let currentPoints = currentSprint.points;
+            totalPoints[currentStatus] += Number(currentPoints);
+        }
+    }
+
+    let donePoints = totalPoints["Done"];
+    let otherPoints = totalPoints["CodeReview"] + totalPoints["InProgress"] + totalPoints["ToDo"];
+
+    console.log(`ToDo: ${totalPoints["ToDo"]}pts`);
+    console.log(`In Progress: ${totalPoints["InProgress"]}pts`);
+    console.log(`Code Review: ${totalPoints["CodeReview"]}pts`);
+    console.log(`Done Points: ${totalPoints["Done"]}pts`);
+
+    if (donePoints >= otherPoints) {
+        console.log("Sprint was successful!")
+    } else {
+        console.log("Sprint was unsuccessful...")
+    }
 }
 
 
 solve(
     [
-        '4',
-        'Kiril:BOP-1213:Fix Typo:Done:1',
-        'Peter:BOP-1214:New Products Page:In Progress:2',
-        'Mariya:BOP-1215:Setup Routing:ToDo:8',
-        'Georgi:BOP-1216:Add Business Card:Code Review:3',
-        'Add New:Sam:BOP-1237:Testing Home Page:Done:3',
-        'Change Status:Georgi:BOP-1216:Done',
-        'Change Status:Will:BOP-1212:In Progress',
-        'Remove Task:Georgi:3',
-        'Change Status:Mariya:BOP-1215:Done',
+        '5',
+        'Kiril:BOP-1209:Fix Minor Bug:ToDo:3',
+        'Mariya:BOP-1210:Fix Major Bug:In Progress:3',
+        'Peter:BOP-1211:POC:Code Review:5',
+        'Georgi:BOP-1212:Investigation Task:Done:2',
+        'Mariya:BOP-1213:New Account Page:In Progress:13',
+        'Add New:Kiril:BOP-1217:Add Info Page:In Progress:5',
+        'Change Status:Peter:BOP-1290:ToDo',
+        'Remove Task:Mariya:1',
+        'Remove Task:Joro:1',
     ]
 )
