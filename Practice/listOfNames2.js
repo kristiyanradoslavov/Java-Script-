13c071eb98a4e320f567a5012037f49195fe6a15
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-09-28 19:10:52 +0300

    commit next task

---
+++ Practice/listOfNames2.js
@@ -0,0 +1,12 @@
+function solution(arr) {
+    let firstRes = [...arr]
+    let result = arr.sort((first, second) => first.localeCompare(second))
+
+    for (let i = 0; i < result.length; i++) {
+        console.log(`${i + 1}.${result[i]}`)
+    }
+}
+
+solution(
+    ["John", "Bob", "Christina", "Ema"]
+)
