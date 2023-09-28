00de6c4abf226a7a0311628e8b2b6bd799252c94
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-09-28 18:49:31 +0300

    commit new changes to the repo

--- Practice/arrayRotation2.js
+++ Practice/arrayRotation2.js
@@ -1,12 +1,13 @@
 function solution(arr, rotations) {
     let result = arr
     for (let currentRot = 0; currentRot < rotations; currentRot++) {
-        let currentNum = result.shift
-        result.push
+        let currentNum = result.shift()
+        result.push(currentNum)
     } 
 
-    console.log(result.join(', '))
+    console.log(result.join(' '))
 }
 
 
 solution([51, 47, 32, 61, 21], 2)
+solution([2, 4, 15, 31], 5)
