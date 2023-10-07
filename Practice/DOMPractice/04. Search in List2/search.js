ad31c08a81495402aa4347a118922af590231688
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-10-07 10:47:08 +0300

    commit new task to the repo

--- Practice/DOMPractice/04. Search in List2/search.js
+++ Practice/DOMPractice/04. Search in List2/search.js
@@ -1,3 +1,22 @@
 function search() {
-   // TODO
+   let townList = document.getElementById('towns');
+   let searchedText = document.getElementById('searchText');
+   let result = document.getElementById('result');
+
+   let totalCount = 0;
+
+   for (let i = 0; i < townList.children.length; i++) {
+      let currentTown = townList.children[i];
+
+      if (currentTown.textContent.includes(searchedText.value)) {
+         currentTown.style.textDecoration = 'underline';
+         currentTown.style.fontWeight = 'bold';
+         totalCount ++;
+      } else {
+         currentTown.style.textDecoration = 'none';
+         currentTown.style.fontWeight = '200';
+      }
+   }
+
+   result.textContent = `${totalCount} matches found`;
 }
