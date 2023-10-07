75313cb229c1a556f3b43f894a09e1c33d720d12
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-10-07 23:58:20 +0300

    commit new task

--- Practice/DOMManipulation/02. Time-Converter2/solution.js
+++ Practice/DOMManipulation/02. Time-Converter2/solution.js
@@ -1,4 +1,50 @@
 function attachEventsListeners() {
+    let inputFields = {
+        days: document.getElementById('days'),
+        hours: document.getElementById('hours'),
+        minutes: document.getElementById('minutes'),
+        seconds: document.getElementById('seconds'),
+    }
 
-    console.log('TODO:...');
+    let allButtons = Array.from(document.querySelectorAll("input[type=button]"));
+
+    for (const currentBtn of allButtons) {
+        currentBtn.addEventListener('click', btnHandler)
+    }
+
+    function btnHandler() {
+        let newDays = 0;
+        let newHours = 0;
+        let newMinutes = 0;
+        let newSeconds = 0;
+
+        let enteredDays = inputFields.days.value ? inputFields.days.value : 0;
+        let enteredHours = inputFields.hours.value ? inputFields.hours.value : 0;
+        let enteredMinutes = inputFields.minutes.value ? inputFields.minutes.value : 0;
+        let enteredSeconds = inputFields.seconds.value ? inputFields.seconds.value : 0;
+
+        if (enteredDays) {
+            newDays = enteredDays;
+            newHours = enteredDays * 24;
+            newMinutes = enteredDays * 24 * 60;
+            newSeconds = enteredDays * 24 * 60 * 60;
+        } else if (enteredHours) {
+            newDays = enteredHours / 24;
+            newMinutes = enteredHours * 60;
+            newHours = enteredHours;
+            newSeconds = enteredHours * 60 * 60;
+        } else if (enteredMinutes) {
+            newDays = enteredMinutes / 60 / 24;
+            newMinutes = enteredHours * 60;
+            newHours = enteredHours;
+            newSeconds = enteredHours * 60 * 60;
+        }
+
+        inputFields.days.value = newDays
+        inputFields.hours.value = newHours;
+        inputFields.minutes.value = newMinutes;
+        inputFields.seconds.value = newSeconds;
+
+    }
+
 }
